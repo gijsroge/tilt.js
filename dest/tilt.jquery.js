5d484ad21868bc53ec1e4d13510ecaab7d4c823f
Gijs <rogegijs@gmail.com>
2017-05-12 21:16:38 +0200

    Added umd wrapper #20

--- dest/tilt.jquery.js
+++ dest/tilt.jquery.js
@@ -1,6 +1,33 @@
 'use strict';
 
-(function ($) {
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function (root, jQuery) {
+            if (jQuery === undefined) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if (typeof window !== 'undefined') {
+                    jQuery = require('jquery');
+                } else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+})(function ($) {
     $.fn.tilt = function (options) {
 
         /**
@@ -272,5 +299,7 @@
      * Auto load
      */
     $('[data-tilt]').tilt();
-})(jQuery);
+
+    return true;
+});
 //# sourceMappingURL=tilt.jquery.js.map
